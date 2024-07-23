8cc845d2ac53a87fa12a270cc32f67df73b1fce8
Ryzald <rizaldiwahyusaputra99@gmail.com>
2024-07-23 15:27:15 +0700

    add: OOP class and object

---
+++ src/main/kotlin/oop/KotlinClass.kt
@@ -0,0 +1,5 @@
+package org.example.oop
+
+//Class: sebuah blueprint yang terdapat properti dan fungsi di dalamnya.
+//Properties: karakteristik dari sebuah kelas, memiliki tipe data.
+//Methods: kemampuan atau aksi dari sebuah kelas.
---
+++ src/main/kotlin/oop/ObjectEveryWhere.kt
@@ -0,0 +1,10 @@
+package org.example.oop
+
+// nilai primitve meruopakah sebuah object yang dapat mengakses funsi dan property suatu class
+
+fun main() {
+    val name = "learnWithRizal"
+
+    // name adalah string sehingga dapat mengakses semua funtion dan properti dari class String
+    println(name.length)
+}
